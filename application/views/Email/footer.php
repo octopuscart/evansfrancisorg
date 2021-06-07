e55cc41ca643d40536c9746d94f687a50325e7e1
octopuscart <dev@develop.com>
2021-06-07 09:38:15 +0530

    cahgns

---
+++ application/views/Email/footer.php
@@ -0,0 +1,51 @@
+<table style="width:100%;background: #282c32;color:white;">
+    <tbody>
+
+        <tr>
+            <td style=" padding:10px 20px;width: 60%;">
+                <div style="float:left"">
+                    <img src="https://www.evansfrancis.org/assets/writer/images/logo.png" style="    height: 60px; margin: 5px 0px 20px;" />
+                    <p>
+                        An evangelist, author, teacher and lyricist with uncompromising faithfulness to the Holy Scriptures.
+                    </p>
+
+                    <span style="float: left; font-size: 12px;"> </span></div>
+            </td>
+            <td style="width: 40%; padding:0px 20px;">
+                <p style="width: 100%;float:left"><span style="float: left; font-size: 12px;">
+
+                        <b>Address </b><br />
+                        Nagpur, Maharashtra, India<br/>
+
+                        <b>Mob.#</b>:&nbsp;+91-9960877313<br />
+                        <b>Email</b>: contact@evansfrancis.org<br />
+                        <b>Web</b>: www.evansfrancis.org</span>
+                </p>
+
+                <p>
+                    <a href="https://www.facebook.com/evansfrancis831" target="_blank" class="socialicons">
+                        <img src="https://www.evansfrancis.org/assets/images/social/facebook.png"/>
+                    </a>
+                    <a href="https://www.instagram.com/evansfrancis831" target="_blank" class="socialicons">
+                        <img src="https://www.evansfrancis.org/assets/images/social/instagram.png"/>
+                    </a>
+                    <a href="https://www.twitter.com/evansfrancis831" target="_blank" class="socialicons">
+                        <img src="https://www.evansfrancis.org/assets/images/social/twitter.png"/>
+                    </a>
+                    <a href="https://www.youtube.com/user/evans240288" target="_blank" class="socialicons">
+                        <img src="https://www.evansfrancis.org/assets/images/social/youtube.png"/>
+                    </a>
+
+                </p>
+            </td>
+
+        </tr>
+    </tbody>
+</table>
+
+</div>
+
+</div>
+</body>
+
+</html>
---
+++ application/views/Email/header.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
+
+    <head>
+        <meta name="viewport" content="width=device-width" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Evans Francis</title>
+        <style>
+            td, th {
+                padding: 0;
+                vertical-align: baseline;
+            }
+            .socialicons{
+                height: 50px;
+                /* float: left; */
+                position: inherit;
+                width:35px;
+                display: inline-block;
+                margin-right: 10px;
+            }
+
+            .socialicons img {
+                width: 35px;
+
+            }
+        </style>
+    </head>
+
+    <body style="margin:0px; background: #f8f8f8; ">
+        <div width="100%" style="background: #f8f8f8; padding: 0px 0px; font-family:arial; line-height:28px; height:100%;  width: 100%; color: #514d6a;">
+            <div style="max-width: 700px; padding:50px 0;  margin: 0px auto; font-size: 14px">
+                <table border="0" cellpadding="0" cellspacing="0" style="width: 100%; background: #282c32">
+                    <tbody>
+                        <tr>
+                            <td style="vertical-align: top; padding-bottom:30px;" align="center">
+                                <a href="<?php echo site_url("/"); ?>" target="_blank">
+                                    <img src="https://www.evansfrancis.org/assets/writer/images/logo.png" alt="Professional Movers" style="border:none;height:50px;    margin-top: 20px;"/></a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
