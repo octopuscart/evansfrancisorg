75e9af1ed6b6cd837f74e7ce2d6a7374758613de
octopuscart <dev@develop.com>
2021-09-28 19:36:19 +0530

    cahgnes

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -55,6 +55,19 @@
             array("title" => "CONTACT"),
         ];
         ?>
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+            (function () {
+                var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+                s1.async = true;
+                s1.src = 'https://embed.tawk.to/615320a825797d7a89013d34/1fgmbf583';
+                s1.charset = 'UTF-8';
+                s1.setAttribute('crossorigin', '*');
+                s0.parentNode.insertBefore(s1, s0);
+            })();
+        </script>
+        <!--End of Tawk.to Script-->
 
 
     </head>
