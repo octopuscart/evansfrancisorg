08e74ed850b5562df36452ecd426fa8c775b7774
octopuscart <octopuscartltd@gmail.com>
2025-02-02 04:40:59 +0530

    Error fix

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -62,7 +62,7 @@ class Pages extends CI_Controller
         $query = $this->db->get_where('download_tokens', ['token' => $token, 'expires_at >=' => date('Y-m-d H:i:s')]);
         if ($query->num_rows() > 0) {
             $bookobj = $this->bookdata[$bookId];
-
+            $file_encoded = __DIR__ . "/../../assets/books/" . $bookobj["bookfile"];
             // Token is valid, proceed with the download
             $filePath = $file_encoded;
             $this->load->helper('download');
